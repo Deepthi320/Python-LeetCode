6d4a33b5c8cadaa370e78c02f865595120c295cc
Deepthi320 <deepthireddym0907@gmail.com>
2026-09-10 20:03:40 +0530

    Add Fizz Buzz solution

--- 01_Python_Basics/001_Fizz_Buzz.py
+++ 01_Python_Basics/001_Fizz_Buzz.py
@@ -1,4 +1,13 @@
-#give in python3 leetcode without comment lines
-
-
-#explain question and code
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        answer = []
+        for i in range(1, n + 1):
+            if i % 3 == 0 and i % 5 == 0:
+                answer.append("FizzBuzz")
+            elif i % 3 == 0:
+                answer.append("Fizz")
+            elif i % 5 == 0:
+                answer.append("Buzz")
+            else:
+                answer.append(str(i))
+        return answer
